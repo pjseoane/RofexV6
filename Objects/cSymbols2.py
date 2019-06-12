154bde22f798f44b331d68e2f015ef48b474052e
Paulino Seoane <pjseoane@gmail.com>
2019-06-12 00:06:48 -0300

    Initial commit. Suscribe MD en symbols2 1 contrato

--- Objects/cSymbols2.py
+++ Objects/cSymbols2.py
@@ -7,134 +7,138 @@ from time import sleep
 
 from RofexConnect import cRESTconnect as rfx
 
-class cSymbol(rfx.cRESTconnect):
+class cSymbols(rfx.cRESTconnect):
 
-    def __init__(self, symbol, marketID):
+    def __init__(self, symbols, marketID='ROFX'):
 
         super().__init__(marketID)
-        self.symbol = symbol
+        self.symbols = symbols
         self.numMessages = 0
-        self.symDetails = self.instrumentDetail(self.symbol)
+        #self.symDetails = self.instrumentDetail(self.symbol)
         self.type_= "smd"
         self.level_ = "1"
         self.msg=""
         self.ws=""
+        self.tickerMsg = ""
+        self.marketDataDict = {}
+        self.contractDetail = {}
+        self.symbol=""
 
-    def getDetails(self):
-        return self.symDetails
-
-    def getContractMultiplier(self):
-        return self.symDetails['instrument']['contractMultiplier']
-
-    def getLowLimitPrice(self):
-        return self.symDetails['instrument']['lowLimitPrice']
-
-    def getHighLimitPrice(self):
-        return self.symDetails['instrument']['highLimitPrice']
-
-    def getMinPriceIncrement(self):
-        return self.symDetails['instrument']['minPriceIncrement']
-
-    def getMaturityDate(self):
-        return self.symDetails['instrument']['maturityDate']
-
-    def getMinTradeVol(self):
-        return self.symDetails['instrument']['minTradeVol']
-
-    def getMaxTradeVol(self):
-        return self.symDetails['instrument']['maxTradeVol']
-
-    def getTickSize(self):
-        return self.symDetails['instrument']['tickSize']
-
-    def getRoundLot(self):
-        return self.symDetails['instrument']['roundLot']
-
-    def getPriceConvertionFactor(self):
-        return self.symDetails['instrument']['priceConvertionFactor']
-
-    def getMarketSegmentId(self):
-        return self.symDetails['instrument']['segment']['marketSegmentId']
-
-    def getCurrency(self):
-        return self.symDetails['instrument']['currency']
-
-    def getCficode(self):
-        return self.symDetails['instrument']['cficode']
-
-    def getMarketId(self):
-        return self.symDetails['instrument']['instrumentId']['marketId']
-
-    def getSymbol(self):
-        return self.symDetails['instrument']['instrumentId']['symbol']
+    # def getDetails(self, sym):
+    #     return self.instrumentDetail(sym)
+    #
+    # def getContractMultiplier(self, sym):
+    #     return self.getDetails(sym)['instrument']['contractMultiplier']
+    #
+    # def getLowLimitPrice(self, sym):
+    #     return self.getDetails(sym)['instrument']['lowLimitPrice']
+    #
+    # def getHighLimitPrice(self):
+    #     return self.symDetails['instrument']['highLimitPrice']
+    #
+    # def getMinPriceIncrement(self):
+    #     return self.symDetails['instrument']['minPriceIncrement']
+    #
+    # def getMaturityDate(self):
+    #     return self.symDetails['instrument']['maturityDate']
+    #
+    # def getMinTradeVol(self):
+    #     return self.symDetails['instrument']['minTradeVol']
+    #
+    # def getMaxTradeVol(self):
+    #     return self.symDetails['instrument']['maxTradeVol']
+    #
+    # def getTickSize(self):
+    #     return self.symDetails['instrument']['tickSize']
+    #
+    # def getRoundLot(self):
+    #     return self.symDetails['instrument']['roundLot']
+    #
+    # def getPriceConvertionFactor(self):
+    #     return self.symDetails['instrument']['priceConvertionFactor']
+    #
+    # def getMarketSegmentId(self):
+    #     return self.symDetails['instrument']['segment']['marketSegmentId']
+    #
+    # def getCurrency(self):
+    #     return self.symDetails['instrument']['currency']
+    #
+    # def getCficode(self):
+    #     return self.symDetails['instrument']['cficode']
+    #
+    # def getMarketId(self):
+    #     return self.symDetails['instrument']['instrumentId']['marketId']
+    #
+    # def getSymbol(self):
+    #     return self.symDetails['instrument']['instrumentId']['symbol']
 
     #*********************************
 
-    def getMD(self) -> object:
-        return self.getMarketData(self.marketId, self.symbol, "1")
-
-    def getBidPrice(self, mdJSON):
-        try:
-            m = mdJSON['marketData']['BI'][0]['price']
-        except:
-            m = 0
-        return m
-
-    def getBidPrice2(self):
-        try:
-            m=self.getBidPrice(self.getMD())
-        except:
-            m=0
-        return m
-
-    def getBidSize(self, mdJSON):
-        try:
-            m = mdJSON['marketData']['BI'][0]['size']
-        except:
-            m = 0
-        return m
-
-    def getBidSize2(self):
-        try:
-            m = self.getBidSize(self.getMD())
-        except:
-            m = 0
-        return m
-
-    def getOfferPrice(self, mdJSON):
-        try:
-            m = mdJSON['marketData']['OF'][0]['price']
-        except:
-            m = 0
-        return m
-
-    def getOfferSize(self, mdJSON):
-        try:
-            m = mdJSON['marketData']['OF'][0]['size']
-        except:
-            m = 0
-        return m
-
-    def getLastPrice(self, mdJSON):
-        try:
-            m = mdJSON['marketData']['LA']['price']
-        except:
-            m = 0
-        return m
-
-    def getClose(self, mdJSON):
-        try:
-            m = mdJSON['marketData']['CL']['price']
-        except:
-            m = 0
-        return m
-
-    def getOpenInterest(self, mdJSON):
-        try:
-            m = mdJSON['marketData']['OI']['size']
-        except:
-            m = 0
-        return m
+    # def getMD(self) -> object:
+    #     return self.getMarketData(self.marketId, self.symbol, "1")
+    #
+    # def getBidPrice(self, mdJSON):
+    #     try:
+    #         m = mdJSON['marketData']['BI'][0]['price']
+    #     except:
+    #         m = 0
+    #     return m
+    #
+    # def getBidPrice2(self):
+    #     try:
+    #         m=self.getBidPrice(self.getMD())
+    #     except:
+    #         m=0
+    #     return m
+    #
+    # def getBidSize(self, mdJSON):
+    #     try:
+    #         m = mdJSON['marketData']['BI'][0]['size']
+    #     except:
+    #         m = 0
+    #     return m
+    #
+    # def getBidSize2(self):
+    #     try:
+    #         m = self.getBidSize(self.getMD())
+    #     except:
+    #         m = 0
+    #     return m
+    #
+    # def getOfferPrice(self, mdJSON):
+    #     try:
+    #         m = mdJSON['marketData']['OF'][0]['price']
+    #     except:
+    #         m = 0
+    #     return m
+    #
+    # def getOfferSize(self, mdJSON):
+    #     try:
+    #         m = mdJSON['marketData']['OF'][0]['size']
+    #     except:
+    #         m = 0
+    #     return m
+    #
+    # def getLastPrice(self, mdJSON):
+    #     try:
+    #         m = mdJSON['marketData']['LA']['price']
+    #     except:
+    #         m = 0
+    #     return m
+    #
+    # def getClose(self, mdJSON):
+    #     try:
+    #         m = mdJSON['marketData']['CL']['price']
+    #     except:
+    #         m = 0
+    #     return m
+    #
+    # def getOpenInterest(self, mdJSON):
+    #     try:
+    #         m = mdJSON['marketData']['OI']['size']
+    #     except:
+    #         m = 0
+    #     return m
 
     def suscribeMD(self):
         headers = {'X-Auth-Token:{token}'.format(token=self.token)}
@@ -156,9 +160,22 @@ class cSymbol(rfx.cRESTconnect):
             sleep(1)
             conn_timeout -= 1
         else:
-            print("cSymbols2 - Sent Suscription msg for: ", self.symbol)
-            self.ws.send(self.buildMessage())
-            sleep(1)
+
+            for self.symbol in self.symbols:
+                print("cSymbols2 - Sent Suscription msg for: ", self.symbol)
+
+                self.contractDetail[self.symbol] = self.instrumentDetail(self.symbol)
+                self.ws.send(self.buildMessage())
+                sleep(1)
+
+
+    def buildMessage(self):
+
+        # 'BI', 'OF', 'LA', 'OP', 'CL', 'SE', 'OI'
+        return "{\"type\":\"" + self.type_ + "\",\"level\":" + self.level_ + \
+               ", \"entries\":[\"BI\", \"OF\",\"LA\"],\"products\":[{\"symbol\":\"" + \
+               self.symbol + "\",\"marketId\":\"" + self.marketId + "\"}]}"
+
 
     def on_message(self, message):
 
@@ -168,11 +185,15 @@ class cSymbol(rfx.cRESTconnect):
             msgType = self.msg['type'].upper()
 
             if msgType == 'MD':
+                self.tickerMsg = self.msg['instrumentId']['symbol']
+                self.marketDataDict[self.tickerMsg] = self.msg
 
-                try:
-                    self.goRobot()
-                except:
-                    print("Problem in goRobot()")
+                if self.marketDataDict.__len__() == len(self.symbols):
+                    print("cYmbols2 - Dict OK, New Msg")
+                    try:
+                        self.goRobot()
+                    except:
+                        print("Problem in goRobot()")
 
             elif msgType == 'OR':
                 print("En Mensaje OR")
@@ -196,12 +217,7 @@ class cSymbol(rfx.cRESTconnect):
         # pass
         print("WS Conection Open...")
 
-    def buildMessage(self):
 
-        # 'BI', 'OF', 'LA', 'OP', 'CL', 'SE', 'OI'
-        return "{\"type\":\"" + self.type_ + "\",\"level\":" + self.level_ + \
-               ", \"entries\":[\"BI\", \"OF\",\"LA\"],\"products\":[{\"symbol\":\"" + \
-               self.symbol + "\",\"marketId\":\"" + self.marketId + "\"}]}"
 
     def goRobot(self):
         # Overridable Method
@@ -214,35 +230,18 @@ class cSymbol(rfx.cRESTconnect):
 
 if __name__ == '__main__':
     print("V6. Class cSymbol")
-    symb = cSymbol("RFX20Jun19", "ROFX")
-    print("JSON", symb.getDetails())
-    print("Market Segment", symb.getMarketSegmentId())
-    print("Low Limit", symb.getLowLimitPrice())
-    print("High Limit", symb.getHighLimitPrice())
-    print("Min Price Incr", symb.getMinPriceIncrement())
-    print("Min Trade Vol", symb.getMinTradeVol())
-    print("Max Trade Vol", symb.getMaxTradeVol())
-    print("Tick Size", symb.getTickSize())
-    print("Multiplier", symb.getContractMultiplier())
-    print("Round lot", symb.getRoundLot())
-    print("Price Convertion Factor", symb.getPriceConvertionFactor())
-    print("Maturity Date", symb.getMaturityDate())
-    print("Currency", symb.getCurrency())
-    print("cficode", symb.getCficode())
-    print("Market Id", symb.getMarketId())
-    print("Symbol", symb.getSymbol())
 
-    md = symb.getMD()
-    print("Symbol MD", md)
+    ticker1 = "DOJun19"
+    ticker2 = "RFX20Jun19"
 
-    print("Symbol Bid/Size :", symb.getBidPrice(md), symb.getBidSize(md))
-    print("Symbol Bid/Size2:", symb.getBidPrice2(), symb.getBidSize2())
-    print("Symbol Offer/Size:", symb.getOfferPrice(md), symb.getOfferSize(md))
-    print("Symbol Last:", symb.getLastPrice(md))
-    print("Symbol OI:", symb.getOpenInterest(md))
-    print("Symbol CL:", symb.getClose(md))
+    tuple = (ticker1, ticker2)
+    md1 = cSymbols(tuple, "ROFX")
+    md1.suscribeMD()
+    print("Ticker:", ticker1)
+    print("Contract Details     :", md1.getDetails(ticker1))
+    print("Contract Multiplier  :", md1.getContractMultiplier(ticker1))
+    print("Contract Low Limit Price:", md1.getLowLimitPrice(ticker1))
 
-    symb2 = cSymbol("DOJun19", "ROFX")
-    marketDataDict={}
-    print("Market suscribe 1:", symb.suscribeMD())
-    print("Market suscribe 2:", symb2.suscribeMD())
+
+
+
